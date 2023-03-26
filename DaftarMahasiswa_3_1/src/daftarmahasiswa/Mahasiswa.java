8e9fbd4300be48d9cb36e260e56d7a7563543791
Zahra Fitria Maharani <100985868+zahraftrm@users.noreply.github.com>
2023-03-26 22:00:55 +0700

    Add files via upload

---
+++ DaftarMahasiswa_3_1/src/daftarmahasiswa/Mahasiswa.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daftarmahasiswa;
+
+/**
+ *
+ * @author sekar
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    private String gender;
+    
+    public Mahasiswa(String nim, String nama, String nilai, String gender){
+        this.nim = nim;
+        this.nama = nama;
+        this.nilai = nilai;
+        this.gender = gender;
+    }
+    
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public void setNilai(String nilai) {
+        this.nilai = nilai;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public String getNim() {
+        return this.nim;
+    }
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public String getNilai() {
+        return this.nilai;
+    }
+    
+    public String getGender() {
+        return this.gender;
+    }
+}
